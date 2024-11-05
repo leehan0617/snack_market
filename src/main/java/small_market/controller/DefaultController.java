0ea9eb1921425706256b67ba1d99738e2d43e6bc
hanbin LEE <leehan0617@naver.com>
2024-11-05 13:05:15 +0900

    commit test

--- src/main/java/small_market/controller/DefaultController.java
+++ src/main/java/small_market/controller/DefaultController.java
@@ -1,7 +1,10 @@
 package small_market.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class DefaultController {
@@ -9,5 +12,32 @@ public class DefaultController {
     @GetMapping
     public String healthCheck() {
         return "Hello";
+    }
+
+    @PostMapping
+    public ResponseEntity<String> postCheck() {
+        return ResponseEntity.ok("POST PASS");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateCheck(@RequestParam String id) {
+        return ResponseEntity.ok("PUT PASS" + id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCheck(@RequestParam String id) {
+        return ResponseEntity.ok("DELETE PASS" + id);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<String> getIdCheck(@RequestParam String id) {
+        return ResponseEntity.ok("GET PASS" + id);
+    }
+
+    @GetMapping("/ip/{id}")
+    public ResponseEntity<String> checkIp(@RequestParam String id, HttpServletRequest request) {
+        String ip = Optional.ofNullable(request.getHeader("x-forwarded-for"))
+                .orElse(request.getRemoteAddr());
+        return ResponseEntity.ok("IP Check:" + ip);
     }
 }
